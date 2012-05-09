7d2810143afb13f1fa6bd10cf162fbb6f27ac8d2
Frank Bergmann <frank.bergmann@project-open.com>
2012-05-09 21:16:48 +0000

    - Demo-Data:   Added creation script (empty at the moment)

---
+++ sql/postgresql/intranet-demo-data-create.sql
@@ -0,0 +1,13 @@
+
+-- Add cached fields in projects listing the number
+-- hours in the main projects. The value will be 
+-- null for sub-projects probably.
+
+-- alter table im_projects add column demo_data_planned_hours_cache numeric(12,2);
+-- alter table im_projects add column demo_data_billable_hours_cache numeric(12,2);
+
+
+
+-- Create a new profile
+-- select im_create_profile ('Skill Profile','skill_profile');
+
