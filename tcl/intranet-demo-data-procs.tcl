e2be4c5eb5b7c7dbed7600113848653035734a8e
Frank Bergmann <frank.bergmann@project-open.com>
2012-06-02 16:02:45 +0000

    - Demo-Data:   First employee logging working

--- tcl/intranet-demo-data-procs.tcl
+++ tcl/intranet-demo-data-procs.tcl
@@ -203,8 +203,8 @@ order by day
 }
 
 
-ad_proc im_demo_data_create_projects {
-    -template_project_id:required
+ad_proc im_demo_data_create_project {
+    {-template_id ""}
     {-new_start_date ""}
     {-company_id "" }
     {-project_name "" }
@@ -225,10 +225,23 @@ ad_proc im_demo_data_create_projects {
 	"]
 	set company_id [util::random_list_element $customer_list]
     }
+
+    # Select a random template if not specified
+    if {"" == $template_id} {
+	set template_list [db_list customer_list "
+		select	p.project_id
+		from	im_projects p
+		where	p.parent_id is null and
+			(p.template_p = 't' OR p.project_name like '%Template%') and
+			p.project_status_id not in (select * from im_sub_categories([im_project_status_closed])) and
+			p.project_type_id not in ([im_project_type_task], [im_project_type_ticket])
+	"]
+	set template_id [util::random_list_element $template_list]
+    }
     
     if {"" == $project_name} {
-	set template_project_name [db_string template_name "select project_name from im_projects where project_id = :template_project_id" -default ""]
-	if {"" == $template_project_name} { ad_return_complaint 1 "im_demo_data_create_projects: invalid template #$template_project_id" }
+	set template_project_name [db_string template_name "select project_name from im_projects where project_id = :template_id" -default ""]
+	if {"" == $template_project_name} { ad_return_complaint 1 "im_demo_data_create_projects: invalid template #$template_id" }
 	if {[regexp {^(.*)Template(.*)$} $template_project_name match tail end]} {
 	    set template_project_name [string trim [concat [string trim $tail] " " [string trim $end]]]
 	}
@@ -261,7 +274,7 @@ ad_proc im_demo_data_create_projects {
                    -clone_timesheet_tasks_p 1 \
                    -clone_target_languages_p 0 \
                    -company_id $company_id \
-                   $template_project_id \
+                   $template_id \
                    $project_name \
                    $project_nr \
                    $clone_postfix \
@@ -272,7 +285,8 @@ ad_proc im_demo_data_create_projects {
 	update im_projects set
 		project_name = :project_name,
 		project_nr = :project_nr,
-		project_path = :project_nr
+		project_path = :project_nr,
+		template_p = 'f'
 	where project_id = :cloned_project_id      
     "
 
@@ -281,6 +295,7 @@ ad_proc im_demo_data_create_projects {
     set move_days [db_string move_days "select :new_start_date::date - p.start_date::date from im_projects p where project_id = :cloned_project_id"]
     db_dml move_cloned_project "
 	    update im_projects set
+	    	project_status_id = [im_project_status_open],
 		start_date = start_date + :move_days * '1 day'::interval,
 		end_date = end_date + :move_days * '1 day'::interval
 	    where
@@ -296,8 +311,85 @@ ad_proc im_demo_data_create_projects {
     return $cloned_project_id
 }
 
+
+
+ad_proc im_demo_data_staff_project {
+    -project_id:required
+} {
+    Assigns the project tasks to "suitable" employees.
+} {
+    # -----------------------------------------------------
+    # Get the list of profiles used in the project
+    set profile_sql "
+    	select	sum(coalesce(bom.percentage, 100.0) * t.planned_units) as planned_units,
+		r.object_id_two as profile_id,
+		r.object_id_one as task_id
+	from	im_projects main_p,
+		im_projects p
+		LEFT OUTER JOIN im_timesheet_tasks t ON (p.project_id = t.task_id)
+		LEFT OUTER JOIN acs_rels r ON (p.project_id = r.object_id_one)
+		LEFT OUTER JOIN im_biz_object_members bom ON (r.rel_id = bom.rel_id)
+	where	main_p.project_id = 32605 and
+		p.tree_sortkey between main_p.tree_sortkey and tree_right(main_p.tree_sortkey) and
+		r.object_id_two not in (select min(user_id) from users where user_id > 0)
+	group by
+		r.object_id_two, r.object_id_one
+	order by
+		task_id, profile_id
+    "
+    db_foreach emps $profile_sql {
+	set profile_hash($profile_id) $availability
+    }
+
+
+    # -----------------------------------------------------
+    # Get the list of employees together with their availability
+    set employee_sql "
+    	select	  e.employee_id,
+		  e.availability
+	from	  im_employees e,
+		  cc_users u
+	where	  u.user_id = e.employee_id and
+		  e.employee_id in (select member_id from group_distinct_member_map where group_id = [im_employee_group_id])
+   "
+    db_foreach emps $employee_sql {
+	set employee_hash($employee_id) $availability
+    }
+
+    # -----------------------------------------------------
+    # Get the current assignments of the user to projects and tasks
+    set direct_assig_sql "
+    	select	u.user_id,
+		p.project_id,
+		bom.percentage
+	from	im_projects p,
+		acs_rels r,
+		im_biz_object_members bom,
+		users u
+	where	r.rel_id = bom.rel_id and
+		r.object_id_one = p.project_id and
+		r.object_id_two in (
+			select u.user_id from dual union 
+			select group_id from group_element_index gei, membership_rels mr where gei.rel_id = mr.rel_id and gei.element_id = u.user_id
+		) and
+		bom.percentage is not null
+    "
+    db_foreach direct_assig $direct_assig_sql {
+	set key "$user_id-$project_id"
+	set project_direct_assig_hash($key) $percentage
+
+	set direct_assig_sum 0
+	if {[info exists direct_assig_hash($user_id)]} { set direct_assig_sum $direct_assig_hash($user_id) }
+	set direct_assig_sum [expr $direct_assig_sum + $percentage]
+	set direct_assig_hash($user_id) $direct_assig_sum
+    }
+
+    ad_return_complaint 1 "<pre>emp: [array get employee_hash]\nts: [array get ts_hash]\ndirect_assig: [array get direct_assig_hash]\n</pre>"
+
+}
+
 ad_proc im_demo_data_log_timesheet_hours {
-    -day:required
+    {-day ""}
 } {
     Checks demo-data for a single day:
     <ul>
@@ -306,6 +398,21 @@ ad_proc im_demo_data_log_timesheet_hours {
     <li>
     </ul>
 } {
+    if {"" == $day} {
+	set day [db_string default_day "
+		select	max(day)::date + 1
+		from	im_hours
+	" -default ""]
+	if {"" == $day} { set day [db_string now "'select now()::date - 365 from dual"] }
+    }
+
+    # Default user for logging data
+    set admin_user_id [db_string admin_user "select min(user_id) from users where user_id > 0" -default 0]
+
+    # Get the current im_audits.audit_id, so that we can 
+    # move all new audit records to the specified day
+    set start_audit_tz [db_string org_audit_tz "select now() from dual" -default 0]
+
     # -----------------------------------------------------
     # Get the list of employees together with their availability
     set employee_sql "
@@ -341,16 +448,19 @@ ad_proc im_demo_data_log_timesheet_hours {
 		p.project_id,
 		bom.percentage
 	from	im_projects p,
+		im_timesheet_tasks t,
 		acs_rels r,
 		im_biz_object_members bom,
 		users u
-	where	r.rel_id = bom.rel_id and
+	where	p.project_id = t.task_id and
+		r.rel_id = bom.rel_id and
 		r.object_id_one = p.project_id and
 		r.object_id_two in (
 			select u.user_id from dual union 
 			select group_id from group_element_index gei, membership_rels mr where gei.rel_id = mr.rel_id and gei.element_id = u.user_id
 		) and
-		bom.percentage is not null
+		bom.percentage is not null and
+		coalesce(p.percent_completed, 0.0) < 100.0
     "
     db_foreach direct_assig $direct_assig_sql {
 	set key "$user_id-$project_id"
@@ -362,6 +472,134 @@ ad_proc im_demo_data_log_timesheet_hours {
 	set direct_assig_hash($user_id) $direct_assig_sum
     }
 
-    ad_return_complaint 1 "<pre>emp: [array get employee_hash]\nts: [array get ts_hash]\ndirect_assig: [array get direct_assig_hash]\n</pre>"
+
+    # -----------------------------------------------------
+    # All available tasks to work on
+    set open_tasks_sql "
+	select	t.task_id,
+		coalesce(p.percent_completed, 0) as percent_completed
+	from	im_projects p,
+		im_timesheet_tasks t
+	where	p.project_id = t.task_id and
+		coalesce(p.percent_completed, 0) < 100.0 and
+		p.project_status_id in (select * from im_sub_categories([im_project_status_open])) and
+		p.end_date >= :day
+    "
+    db_foreach open_tasks $open_tasks_sql {
+	set open_tasks_hash($task_id) $percent_completed
+    }
+
+
+    foreach uid [array names employee_hash] {
+	ns_log Notice "im_demo_data_log_timesheet_hours: Logging hours for user_id=$uid"
+	
+	# Clean the array from last user
+	array unset uid_hash
+	
+	# Check if there are direct assignments
+	set percent_available $employee_hash($uid)
+	set hours_today [expr round($percent_available * 8.0 * ((1.5 + rand()) / 2.0)) / 100.0]
+
+	# Check if there are direct assignments for this user
+	foreach tuple [array names project_direct_assig_hash] {
+	    regexp {([^\-])\-([^\-])} $tuple match tuple_uid tuple_pid
+	    if {$uid == $tuple_uid} {
+	        set uid_hash($tuple_pid) $project_direct_assig_hash($tuple)
+	    }
+	}
+
+	# Pick two random tasks where something is to do...
+	if {"" == [array get uid_hash]} {
+	    set task_list [array names open_tasks_hash]
+	    set task_1 [util::random_list_element $task_list]
+	    set task_2 [util::random_list_element $task_list]
+	    set uid_hash($task_1) 50.0
+	    set uid_hash($task_2) 50.0
+	}
+
+	# Normalize the uid_hash
+	set percent_assigned_total 0
+	foreach tid [array names uid_hash] {
+	    set percent_assigned_total [expr $percent_assigned_total + $uid_hash($tid)]
+	}
+
+	ns_log Notice "im_demo_data_log_timesheet_hours: user=$uid will log $hours_today hours on tasks [array get uid_hash]"
+
+	# Log hours on the specified tasks
+	array set modified_projects_hash {}
+	foreach tid [array names uid_hash] {
+
+	    set perc_assig $uid_hash($tid)
+	    set hours [expr $hours_today * $perc_assig / 100.0]
+
+	    # Calculate the advance of the task
+	    db_1row task_info "
+	    	select	coalesce(t.planned_units, 8.0) as task_planned_units,
+			coalesce(p.percent_completed, 0.0) as task_percent_completed
+		from	im_projects p,
+			im_timesheet_tasks t
+		where	p.project_id = t.task_id and
+			t.task_id = :tid
+	    "
+
+	    # Efficiency of work greatly varies between 0.5 and 1.5 ...
+	    set rand_factor [expr (0.5 + rand()) / 1.0 ]
+	    set percent_done [expr 100.0 * $hours / $task_planned_units * $rand_factor]
+	    set new_percent_completed [expr $task_percent_completed + $percent_done]
+	    if {$new_percent_completed > 100.0} { set new_percent_completed 100.0 }
+	    ns_log Notice "im_demo_data_log_timesheet_hours: task(tid=$tid, est=$task_planned_units, compl=$task_percent_completed) gets $percent_done percent completed today"
+
+	    db_dml hours_insert "
+		insert into im_hours (user_id, project_id, day, hours) 
+		values (:uid, :tid, :day, :hours)
+	    "
+
+	    db_dml task_update "
+		update im_projects
+		set percent_completed = :new_percent_completed
+		where project_id = :tid
+	    "
+
+	    # Remember the modified projects
+	    set main_project_id [im_project_main_project $tid]
+	    set modified_projects_hash($tid) $tid
+	    set modified_projects_hash($main_project_id) $main_project_id
+	}
+    }
+
+
+    # Update cache information of projects and audit
+    foreach pid [array names modified_projects_hash] {
+    
+	# Re-calculate the cost cache
+	im_cost_update_project_cost_cache $pid
+	
+	# Update the cost caches
+	im_timesheet_update_timesheet_cache -project_id $pid
+
+	# Calculate the %completed of the project
+	im_timesheet_project_advance $pid
+
+	# Write an audit record
+	# im_audit -object_id $tid -user_id $admin_user_id -action "after_update" -comment "im_demo_data_log_timesheet_hours: Simulating hour logging"
+    }
+
+    # Move all audit records back to the specified day
+    set end_audit_tz [db_string end_audit_tz "select now() from dual" -default 0]
+
+    db_dml shift_audits "
+	update im_audits
+	set audit_date = :day
+	where audit_date between :start_audit_tz and :end_audit_tz
+    "
+
+    db_dml shift_project_audits "
+	update im_projects_audit
+	set last_modified = :day
+	where last_modified between :start_audit_tz and :end_audit_tz
+    "
+    
+
+    ad_return_complaint 1 "<pre>emp: [array get employee_hash]\nts: [array get ts_hash]\ndirect_assig: [array get direct_assig_hash]\ntasks: [array get open_tasks_hash]</pre>"
 
 }
