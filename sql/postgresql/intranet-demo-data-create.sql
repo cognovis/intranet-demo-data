007e31c8b19e32338462cb912089f132caae6389
Frank Bergmann <frank.bergmann@project-open.com>
2012-10-03 15:45:39 +0000

    - Demo-Data:   Added Skill Profile configuration

--- sql/postgresql/intranet-demo-data-create.sql
+++ sql/postgresql/intranet-demo-data-create.sql
@@ -1,13 +1,8 @@
-
--- Add cached fields in projects listing the number
--- hours in the main projects. The value will be 
--- null for sub-projects probably.
-
--- alter table im_projects add column demo_data_planned_hours_cache numeric(12,2);
--- alter table im_projects add column demo_data_billable_hours_cache numeric(12,2);
+-- intranet-demo-date/sql/postgresql/intranet-demo-data-create.sql
+--
+-- Setup "Tigerpond" company
 
 
-
--- Create a new profile
--- select im_create_profile ('Skill Profile','skill_profile');
+\i categories.sql
+\i users-skills.sql
 
